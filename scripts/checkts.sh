f82a8a9ab0b35d7e12dfd34a0703ade41dbf3ee7
Mario Gemoll <mariogemoll@gmx.net>
2025-07-09 15:35:29 +0200

    ESLint the ESLint config

--- scripts/checkts.sh
+++ scripts/checkts.sh
@@ -10,12 +10,14 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cd $SCRIPT_DIR/../widgets
 
+npx eslint -c basic.eslint.config.js basic.eslint.config.js eslint.config.js
 npx eslint src
 
 npx tsc
 
 cd $SCRIPT_DIR/../notebook/widget-wrappers
 
+npx eslint -c basic.eslint.config.js basic.eslint.config.js eslint.config.js
 npx eslint src
 
 npx tsc
--- widgets/eslint.config.js
+++ widgets/eslint.config.js
@@ -1,33 +1,19 @@
-import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import simpleImportSort from "eslint-plugin-simple-import-sort";
+
+import basicConfig from './basic.eslint.config.js';
 
 export default tseslint.config(
-  eslint.configs.recommended,
+  basicConfig,
   tseslint.configs.strictTypeChecked,
   tseslint.configs.stylisticTypeChecked,
   {
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    plugins: {
-      "simple-import-sort": simpleImportSort,
+        tsconfigRootDir: import.meta.dirname
+      }
     },
     rules: {
-      semi: ['error', 'always'],
-      indent: ['error', 2],
-      'max-len': ['error', { code: 100, tabWidth: 2, ignoreUrls: true }],
-      'quotes': ['error', 'single', { avoidEscape: true }],
-      'object-curly-spacing': ['error', 'always'],
-      'space-before-function-paren': ['error', 'never'],
-      'array-bracket-spacing': ['error', 'never'],
-      'eol-last': ['error', 'always'],
-      'no-trailing-spaces': 'error',
-      'linebreak-style': ['error', 'unix'],
-      'comma-dangle': ['error', 'never'],
       '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: false }],
       '@typescript-eslint/consistent-type-imports': 'error',
       '@typescript-eslint/explicit-module-boundary-types': 'error',
@@ -42,18 +28,9 @@ export default tseslint.config(
           'signature',
           'field',
           'constructor',
-          'method',
+          'method'
         ]
-      }],
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-
-      'no-duplicate-imports': 'error',
-      'prefer-const': 'error',
-      'no-var': 'error',
-      'eqeqeq': ['error', 'always'],
-      'curly': ['error', 'all'],
-      'yoda': ['error', 'never']
+      }]
     }
   }
 );
